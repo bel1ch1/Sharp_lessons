20c14939b412a19ccfc08749540e3f09d20a7284
AndreyZ <andreyzv2525@gmail.com>
2023-12-04 17:12:49 +0300

    4.9

--- Program.cs
+++ Program.cs
@@ -1,29 +1,12 @@
 ﻿using System;
-using System.Security.Cryptography;
-using System.Xml.Schema;
-
+using System.Linq;
+using System.Collections.Generic;
 
 
 class Program
 {
     static void Main(string[] args)
     {
-        int K = 9;
-        string[] A = {
-            "ABC12A",
-            "DE4FGH",
-            "5678",
-            "H5IJKL",
-            "M5NOP",
-            "Q35RST"
-        };
 
-        var seq1 = A.Select(val => String.Join("", val.Take(K).ToList())); // выбираем все элементы из строк до К
-        var seq2 = A.SkipWhile(char.IsDigit); // выбираем все элементы после последней цыфры
-
-        var result = seq1.Intersect(seq2);
-
-        foreach(string i in result)
-            Console.WriteLine(i);
     }
 }
