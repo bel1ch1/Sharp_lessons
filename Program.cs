318916eb177d60844f074587fabe698b187e6063
AndreyZ <andreyzv2525@gmail.com>
2024-02-17 18:32:45 +0300

    test

--- Program.cs
+++ Program.cs
@@ -2,32 +2,21 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.IO;
 
 class Program
 {
     static void Main(string[] args)
     {
-    path_to_json = "./struct.json" // Путь до json файла
-
-    new_mark a = new new_mark("Algebra", 5);
-    a.add_mark_2_json();
+        using (FileStream fs = new FileStream("struct.json", FileMode.OpenOrCreate))
+        {
+            New_mark a = new New_mark(1, 2, 4);
+            await JsonSerializer.Serialization<New_mark>(fs, a);
+            Console.WriteLine("Data has been saved to file");
+        }
 
     }
 
-}
 
-class new_mark
-{
-    public string subject { get; set; }
-    public int mark { get; set; }
-    public new_mark(string subject, int mark)
-    {
-        this.mark = mark;
-        this.subject = subject;
-    }
-    public string add_mark_2_json(int mark, string subject, new_mark a)
-    {
-        string json = JsonSerializer.Serialize(a.subject, a.mark);
-        return json;
-    }
 }
--- models/Currency.cs
+++ models/Currency.cs
@@ -27,4 +27,20 @@ namespace Sharp_lessons.models
             public string id_subj {get; set;}
             public string grade {get; set;}
         }
+
+    class New_mark
+    {
+        [JsonPropertyName("id_stud")]
+        public int Id_stud { get; set; }
+        [JsonPropertyName("id_subj")]
+        public string Id_subj { get; set; }
+        [JsonPropertyName("grade")]
+        public int Grade { get; set; }
+
+        public New_mark(int id_stud ,int id_subj, int grade)
+        {
+            this.Id_stud = Id_stud;
+            this.Id_subj = Id_subj;
+            this.Grade = Grade;
+        }
 }
